0bfba3bdddb4d7e7c486dbe7d10ceb5fbc116242
frostycakes <lamquoc.24@gmail.com>
2022-10-21 16:46:02 -0400

    landing left display structure

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,19 +5,22 @@ const LandingPage = () => {
 
   return (
     <div className="landing-parent">
-      <div className="landing-title">
-        <p>WhackDADDY</p>
-        <p>TO</p>
-        <p>MacDADDY</p>
+      <div className="landing-left">
+        <div className="landing-title">
+          <p>WhackDADDY</p>
+          <div className="blub"></div>
+          <p>TO</p>
+          <p>MacDADDY</p>
+        </div>
         <p>An evidence-based way to lose weight 💪</p>
-        <div
+        <button
           //TODO className="start-btn"
           onClick={() => {
             setForm(true);
           }}
         >
           CLICK ME <span>→</span>
-        </div>
+        </button>
       </div>
       {form && <form className="landing-form"></form>}
     </div>
