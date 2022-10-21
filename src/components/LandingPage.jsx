5f6a57d86b9335a2f7aa4b061baf5ca38a221574
frostycakes <lamquoc.24@gmail.com>
2022-10-21 16:19:16 -0400

    import font

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LandingPage = () => {
-  return (
-    <div></div>
-  )
-}
+  const [form, setForm] = useState(false);
 
-export default LandingPage
+  return (
+    <div className="landing-parent">
+      <div className="landing-title">
+        <p>WhackDADDY</p>
+        <p>TO</p>
+        <p>MacDADDY</p>
+        <p>An evidence-based way to lose weight 💪</p>
+        <div
+          //TODO className="start-btn"
+          onClick={() => {
+            setForm(true);
+          }}
+        >
+          CLICK ME <span>→</span>
+        </div>
+      </div>
+      {form && <form className="landing-form"></form>}
+    </div>
+  );
+};
+
+export default LandingPage;
