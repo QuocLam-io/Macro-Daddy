42f47cb56c0e47b874fbc038c339d7d897091e8a
frostycakes <lamquoc.24@gmail.com>
2022-10-26 16:45:25 -0400

    navbar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import CardList from "./components/CardList";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { signedIn } = useSelector((store) => store.userBio);
+  
+
   return (
     <div className="App">
       <Navbar />
-      <LandingPage />
+      <Routes>
+        <Route
+          path="/"
+          element={signedIn ? <CardList /> : <LandingPage />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,38 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="nav-parent">
-      <div className="icon">
-        <img src="/imgs/icon.svg" alt="blub" />
-        <p>Macro DADDY</p>
+      <Link to="/" >
+        <div className="icon">
+          <img src="/imgs/icon.svg" alt="blub" />
+          <p>Macro DADDY</p>
+        </div>
+
+        <div className="nav-input" action="">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search Food"
+          // value={searchResults}
+          // onChange={searchResultsHandler}
+          // onKeyDown={handleKeyDown}
+        />
+
+        <button className="mag-btn" type="submit" 
+        // onClick={submitHandler}
+        >
+          <img
+            className="magnify"
+            src={require(`../images/magnify.png`)}
+            alt=""
+          />
+        </button>
       </div>
+
+      <Link to="/userpage">
+        <p className="user-btn">MY MACROS</p>
+      </Link>
+      </Link>
     </div>
   );
 };
