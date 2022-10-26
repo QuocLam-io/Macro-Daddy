4247f9d846d7ac7b27d0d434e44a065084caaa75
frostycakes <lamquoc.24@gmail.com>
2022-10-26 15:53:26 -0400

    logo svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='nav-parent' ></div>
-  )
-}
+    <div className="nav-parent">
+      <div className="icon">
+        <img src="/imgs/icon.svg" alt="blub" />
+        <p>Macro DADDY</p>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
