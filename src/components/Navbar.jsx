e0ff87dfb3ff4c5d7a7db2b5d1de81d84f1dca7a
frostycakes <lamquoc.24@gmail.com>
2022-10-28 19:14:48 -0400

    axios header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Navbar = () => {
   const { signedIn } = useSelector((store) => store.userBio);
 
+  const callAxios = () => {
+    axios
+      .post(
+        `https://trackapi.nutritionix.com/v2/natural/nutrients/`,
+        {
+          query: "pepperoni pizza",
+        },
+        {
+          headers: {
+            "x-app-id": "40b82218",
+            "x-app-key": "0b053788ee5d2365587a6aa568240953",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="nav-parent">
-
       <Link to="/">
         <div className="icon">
           <img src="/imgs/icon.svg" alt="blub" />
@@ -15,29 +38,35 @@ const Navbar = () => {
         </div>
       </Link>
 
-  {signedIn &&    <div className="nav-search" action="">
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="Search Food"
-          // value={searchResults}
-          // onChange={searchResultsHandler}
-          // onKeyDown={handleKeyDown}
-        />
+      {signedIn && (
+        <div className="nav-search" action="">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search Food"
+            // value={searchResults}
+            // onChange={searchResultsHandler}
+            // onKeyDown={handleKeyDown}
+          />
 
-        <button
-          type="submit"
-          // onClick={submitHandler}
-        >
-          <img className="magnify" src="/imgs/magnify.png" alt="" />
-        </button>
-      </div>}
+          <button
+            onClick={(e) => {
+              callAxios();
+            }}
+            type="submit"
+            // onClick={submitHandler}
+          >
+            <img className="magnify" src="/imgs/magnify.png" alt="" />
+          </button>
+        </div>
+      )}
 
-      {signedIn &&<Link className="user-page" to="/userPage">
-        MY MACROS
-      </Link>}
-
+      {signedIn && (
+        <Link className="user-page" to="/userPage">
+          MY MACROS
+        </Link>
+      )}
     </div>
   );
 };
