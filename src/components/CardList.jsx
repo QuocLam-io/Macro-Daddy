2c69e7c09d959b854e8c82c0af8f38116cf0a62e
frostycakes <lamquoc.24@gmail.com>
2022-10-31 16:14:57 -0400

    macro display skeleton

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const CardList = () => {
-  return (
-    <div>CardList</div>
-  )
-}
 
-export default CardList
+  const { bmr } = useSelector((store) => store.userBio);
+
+  return (
+    <div className="cardlist-parent">
+      <div className="search-results-display">
+      </div>
+      <div className="macro-display">
+        <p>MY DAILY MACROS</p>
+        <div className="">{bmr}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
