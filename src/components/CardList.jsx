e042bab5d37b89ad27e0bdeb47d0d8bb62725166
frostycakes <lamquoc.24@gmail.com>
2022-11-01 16:24:00 -0400

    useSelector searchResults reducer

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,15 +3,16 @@ import { useSelector } from "react-redux";
 
 const CardList = () => {
   const { bmr } = useSelector((store) => store.userBio);
+  const { common, branded } = useSelector((store) => store.searchResults);
 
   return (
     <div className="cardlist-parent">
       <div className="search-results-display"></div>
       <div className="macro-display">
         <p>MY DAILY MACROS</p>
-        <div className="bmr-divide">0
-        <span>TDEE</span>  / {bmr} <span>
-             Calories</span></div>
+        <div className="bmr-divide">
+          0<span>TDEE</span> / {bmr} <span>Calories</span>
+        </div>
         <div className="bmr-difference">Difference</div>
         <div className="macro-counts">
           <div className="display-protein"></div>
--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userBioReducer from "./features/userBio/userBioSlice";
 import searchResultsReducer from "./features/searchResults/searchResultsSlice";
-console.log(searchResultsReducer)
-console.log(userBioReducer)
-
 
 export const store = configureStore({
   reducer: {
