dbaaf7055c73e398deade88ae7a7ebe46163ab94
frostycakes <lamquoc.24@gmail.com>
2022-11-02 18:29:45 -0400

    add mealHistorySlice to store

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userBioReducer from "./features/userBio/userBioSlice";
 import searchResultsReducer from "./features/searchResults/searchResultsSlice";
+import mealHistoryReducer from "./features/mealHistory/mealHistorySlice";
 
 export const store = configureStore({
   reducer: {
     userBio: userBioReducer,
     searchResults: searchResultsReducer,
+    mealHistory: mealHistoryReducer,
   },
 });
