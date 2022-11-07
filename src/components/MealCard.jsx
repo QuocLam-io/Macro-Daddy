24af35c1ae5df40286adb372ca1ff3de1f8c09a9
frostycakes <lamquoc.24@gmail.com>
2022-11-07 16:18:07 -0500

    mealCard-parent border

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const MealCard = () => {
-  return (
-    <div className='mealCard-parent' >MealCard</div>
-  )
-}
+  return <div className="mealCard-parent">MealCard</div>;
+};
 
-export default MealCard
+export default MealCard;
