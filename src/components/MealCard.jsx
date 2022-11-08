00027887365e78095b867bc53249becc1c2bd0cf
frostycakes <lamquoc.24@gmail.com>
2022-11-08 17:46:15 -0500

    mealCard delete button

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,4 +1,7 @@
+import { isCursorAtEnd } from "@testing-library/user-event/dist/utils";
 import React from "react";
+import { useSelector } from "react-redux";
+
 
 const MealCard = ({ meal }) => {
   return (
@@ -10,7 +13,17 @@ const MealCard = ({ meal }) => {
       {/* <img src={meal.photo} alt="" /> */}
 
       <div className="mealCard-info">
-        <div className="meal-name">{meal.name}</div>
+        <div className="meal-name">{meal.name} 
+        <div
+          // onClick={} Dispatch() delete meal
+          style={
+            {fontSize: "1.5rem", color: "grey", fontWeight: "normal",
+            cursor: "pointer"
+          }
+            
+        }
+        >X</div>
+        </div>
         <div className="mealCard-macros">
           <div>
             <div>Calories</div>
